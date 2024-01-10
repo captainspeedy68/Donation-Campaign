334b46aebf79eac9e27770598ee2f7e5afff68da
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-01-10 23:02:57 +0600

    donation details page

---
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
+
+const DonationDetails = () => {
+    const donations = useLoaderData();
+    const { id } = useParams();
+    const idInt = parseInt(id);
+    const donation = donations.find(donation => donation.id === idInt)
+    const { picture, price, description, title, text_button_bg } = donation;
+    // console.log(id)
+    const style = {
+        backgroundColor: text_button_bg
+    }
+    return (
+        <div >
+            <div className=''>
+                <div className='relative'>
+                    <div className=''>
+                        <img src={picture} className="w-11/12 h-3/6 " alt="" />
+
+                    </div>
+                    <div className='absolute bottom-0 left-0 right-0 bg-[#0B0B0B80] w-11/12 h-1/5 items-center inline-flex'>
+                        <button className='btn text-white items-center ml-14 rounded-none border-none w-28' style={style}>Donate {price}</button>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className='font-extrabold text-4xl mx-12 my-5'>{title}</div>
+            <div className='font-normal text-base mx-12 my-5'>{description}</div>
+
+        </div>
+    );
+};
+
+export default DonationDetails;
