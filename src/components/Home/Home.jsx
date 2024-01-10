3e3b1561614a0bfcb7c0c9a6ab4ad26b5a5707c5
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-01-10 19:56:23 +0600

    Display donations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
+import DisplayDonations from '../DisplayDonations/DisplayDonations';
 import Search from '../Search/Search';
 
 const Home = () => {
     return (
         <div>
             <Search></Search>
-            <
+            <DisplayDonations></DisplayDonations>
         </div>
     );
 };
